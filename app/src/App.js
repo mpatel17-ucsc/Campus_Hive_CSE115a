ea6cc720799174d58a7ef654fd4542635a8e2bce
Aryaman Gautam <aryamangautamg@gmail.com>
2025-01-27 23:57:03 -0800

    integrate old landing page after sign in

--- app/src/App.js
+++ app/src/App.js
@@ -33,8 +33,18 @@ function App() {
             isLoggedIn ? (
               <div className="App">
                 <header className="App-header">
-                  <h1>Welcome to CampusHive!</h1>
                   <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                  </p>
+                  <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Learn React
+                  </a>
                   <button onClick={() => setIsLoggedIn(false)}>Logout</button>
                 </header>
               </div>
